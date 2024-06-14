f2650be8fe2f98f1949f982db837d3fd94a04e22
Yurcell <chestayurcel@gmail.com>
2024-06-14 14:39:04 +0700

    Implementasi kasus penyisipan (insert) dalam struktur switch-case menu

--- BinarySearchTree_0072.cpp
+++ BinarySearchTree_0072.cpp
@@ -133,5 +133,16 @@ int main()
         char ch;
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word : ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
     }
 }
