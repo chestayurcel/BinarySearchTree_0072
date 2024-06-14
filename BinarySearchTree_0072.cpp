72aae9337748f9c1274683fabdd9143ac27c48a8
Yurcell <chestayurcel@gmail.com>
2024-06-14 14:30:22 +0700

    Implementasi fungsi traversal postorder untuk pohon biner

--- BinarySearchTree_0072.cpp
+++ BinarySearchTree_0072.cpp
@@ -99,4 +99,20 @@ public:
             preorder(ptr->rightchild);
         }
     }
-}
+
+    void postorder(Node* ptr)
+    {
+        // Performs the postorder traversal of the tree
+        if (ROOT == NULL)
+        {
+            cout << "Tree is empty" << endl;
+            return;
+        }
+        if (ptr != NULL)
+        {
+            postorder(ptr->leftchild);
+            postorder(ptr->rightchild);
+            cout << ptr->info << " ";
+        }
+    }
+};
